d1be698035844ffa2d56f10655de0c301aeeebf0
Katarina Slivova <katka1391@gmail.com>
2021-11-15 23:17:45 +0000

    ingredients seeder made

---
+++ resources/views/your-list/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts/main')
+@section('content')
+
+   <div class="container">
+      <h1 class="title" style="font-size: 10rem">Sweet Cuisine</h1>  
+      <h2 class="subTitle">Your List</h2>
+   </div>
+
+   <div class="contact-container">
+
+      <div>
+         <p>sometihng</p>
+         <p>sometihng</p>
+         <p>sometihng</p>
+         <p>sometihng</p>
+         <p>sometihng</p>
+         <p>sometihng</p>
+      </div>
+      
+
+      <div class="contact-right-panel">
+         <div class="carousel-onTheRight blog">
+            <p class="text-decoration-none" > <a href="" class="text-dark" ><u>Search Recipes</u></a> </p>
+            <br>
+            <p class="text-decoration-none"> <a href="" class="text-dark"><u>BREAKFAST  /  MAIN DISH  /  DESSERT</u></a> </p>
+            <br>
+            <p class="text-decoration-none"> <a href="" class="text-dark"><u>VEGAN  /  GLUTEN FREE</u></a> </p>
+               <br>
+               <br>
+            <p  class="text-decoration-none"><a href="" class="text-danger">ALL RECIPES >></a></p>
+            <div class="card" style="width: 18rem;">
+               <img class="card-img-top" src="..." alt="Card image cap">
+               <div class="card-body">
+                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               </div>
+             </div>
+             <br>
+             <p>TAKE A LOOK AT OUR BLOG PAGE...</p>
+             <br>
+             <br>
+          <p class="text-decoration-none"><a href="" class="text-danger">BLOG>></a></p>
+         </div>
+      </div>
+   </div>
+
+
+
+
+      
+@endsection 
+      
+   
+
+
+
+
+
+
+   
+   
