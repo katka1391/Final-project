1cd62a4c1673816ca92d68e3c4241bc224ce0e5b
Your Name <you@example.com>
2021-11-16 21:36:59 +0100

    style

--- resources/views/articles/article3.blade.php
+++ resources/views/articles/article3.blade.php
@@ -13,25 +13,58 @@
 
    </h2>
 </div>
+<div class="article-container">
 
-<div class="article3-container">
-   <div class="article3-up">
+    <h2 style="font-size: 40px">Pumpkin Carving Tips and Tricks</h2>
+    <div class="article-up">
+
+        <br>
+        <q>Now that you know how to choose the perfect Halloween pumpkin 
+            for carving, here are some tips to get the job done!
+            If you plan to use battery-powered lights in your jack-o-lantern, 
+            carve a circle out of the bottom to preserve the top. For candles or 
+            tealights, cut a hole in the top. Make sure you buy a pumpkin with a
+            stem. A thick, green stem works best and indicates the pumpkin 
+            has thick walls as well. Don’t be afraid to pick oddly shaped 
+            pumpkins. Depending on your design, the weirder the shape, the 
+            more unique design you can create. Scoop out as much of the 
+            pumpkin guts as possible. The thinner the walls, the longer your 
+            pumpkin will last. This will also help to delay mold growth. 
+            Research design ideas before you start carving! This prevents 
+            mistakes and allows you to sketch out the design onto the pumpkin 
+            before you begin.</q>
+       
+ 
+      
+    </div>
+    
+        <!-- sliders -->
+       <div class="slider">
+
+        </div>
+
+        <div class="article-down">
+            <q> Pumpkins are packed with skin-friendly nutrients, including vitamins C and E, as well as beta-carotene, all of which play an important role in the health of our skin.
+
+                Vitamin C is not naturally made by the body, so it’s important we get it from our diet, as it plays a part in the formation of collagen which keeps skin plump and firm, vitamin C also helps prevent bruising and promotes wound healing.
+                
+                Vitamin E is an excellent antioxidant and works with vitamin C to help protect against sun damage and dryness. Vitamin A, or beta-carotene, is also involved in skin protection from the sun’s UVB rays and may help protect against sunburn, although sunscreen is still needed!.
+                Low levels of vitamin A has been linked with reduced vision and even blindness. Beta-carotene, as well as vitamins C and E, help protect eyes and reduce the risk of age-related eye diseases. Pumpkin is also an excellent source of two carotenoids called lutein and zeaxanthin, levels of which have been linked to a reduced risk of cataracts.
+                As indicated by their bright orange colour, pumpkins contain beta-carotene, which is converted to vitamin A when consumed. Research has demonstrated that vitamin A plays an important role in promoting immune function. Vitamin C also contributes to immune activity facilitating immune cell activity and increasing white blood cells.</p>    
+
+            </div>
+          
+</div>
 
 
-       <div class="cube-body">
-           <div class="cube-up">
-               <div class="side front-up"></div>
-               <div class="side top-up"></div>
-               <div class="side bottom-up"></div>
-               <div class="side back-up"></div>
-           </div>
-       </div>
 
 
-       <div class="space-between-up__up">
 
-       </div>
-       <div>
+
+<!--<div class="article3-container">
+   
+
+       <div class="article-up">
            <h3> Pumpkin Carving Tips and Tricks</h3>
            <q>Now that you know how to choose the perfect Halloween pumpkin 
             for carving, here are some tips to get the job done!
@@ -48,56 +81,20 @@
             mistakes and allows you to sketch out the design onto the pumpkin 
             before you begin.
             </q>
-           <div class="space-between__middle">
-
-           </div>
-           <div class="border-left"><q> The easiest way to use a pre-made pattern or design you printed 
-            onto paper is to first tape the paper onto the face of the pumpkin. 
-            Then use a fork, pencil, or needle tool to poke holes along the lines 
-            you want to cut out. Pumpkins don’t last forever. For the best-
-            looking jack-o-lantern on the block, carve your pumpkin no more 
-            than two to three days prior to your event or Halloween party. 
-            Light up your jack-o-lantern using an LED or CFL light. Other types
-            of lights, as well as candles, give off heat that can cook the pumpkin
-            form the inside, shortening its lifespan.
-            Make rough design cuts first to remove large pieces from the 
-            gourd. Once you the basics carved out, you can then go back and 
-            clean up the design or add additional details. When cutting your 
-            pumpkin with a knife, use clean up-and-down slices. You want to 
-            avoid making cuts on a slant. Keep your pumpkin scraps to create 
-            additional design elements for your Halloween jack-o-lantern. You 
-            can use these pieces to cut out a tongue, hair accessories, or other 
-            features from leftover chunks of pumpkin shell. Save your seeds! 
-            Pepitas, or pumpkin seeds, are delicious when seasoned and 
-            roasted.</q>
-           </div>
-       </div>
-       <div class="space-between-up__down">
 
        </div>
+    
+       <div class="space-between">
+
+    </div>
+
+       
+
+ 
 
 
-       <div class="cube-body">
-           <div class="cube-down">
-               <div class="side front-down"></div>
-               <div class="side top-down"></div>
-               <div class="side bottom-down"></div>
-               <div class="side back-down"></div>
-           </div>
-       </div>
-
-
-   </div>
-
-
-
-   <div class="article3-down">
-
-
-       <div>
-           <div class="space-between-up__down">
-
-           </div>
+       <div class="article-down">
+         
            <h3 style=" margin: 0;">Health benefits of pumpkin</h3>
            <q> Pumpkins are packed with skin-friendly nutrients, including vitamins C and E, as well as beta-carotene, all of which play an important role in the health of our skin.
 
@@ -106,13 +103,11 @@
             Vitamin E is an excellent antioxidant and works with vitamin C to help protect against sun damage and dryness. Vitamin A, or beta-carotene, is also involved in skin protection from the sun’s UVB rays and may help protect against sunburn, although sunscreen is still needed!.</q>
             Low levels of vitamin A has been linked with reduced vision and even blindness. Beta-carotene, as well as vitamins C and E, help protect eyes and reduce the risk of age-related eye diseases. Pumpkin is also an excellent source of two carotenoids called lutein and zeaxanthin, levels of which have been linked to a reduced risk of cataracts.
             As indicated by their bright orange colour, pumpkins contain beta-carotene, which is converted to vitamin A when consumed. Research has demonstrated that vitamin A plays an important role in promoting immune function. Vitamin C also contributes to immune activity facilitating immune cell activity and increasing white blood cells.
-            <div class="space-between-down__down">
-
-           </div>
+           
        </div>
 
 
-   </div>
-</div>
+   
+</div>-->
 
 @endsection
--- resources/views/articles/article4.blade.php
+++ resources/views/articles/article4.blade.php
@@ -15,7 +15,47 @@
 </div>
 
 <div class="article4-container">
-   <div class="article4-up">
+    <div class="article-container">
+
+        <h2 style="font-size: 40px">What is sourdough?</h2>
+        <div class="article-up">
+    
+            <br>
+            <q>Sourdough is naturally leavened bread, which means it doesn’t use 
+                commercial yeast to rise. Instead, it uses a ‘starter’ – a fermented 
+                flour and water mixture that contains wild yeast and good bacteria 
+                – to rise. This also produces the tangy flavour and slightly chewy 
+                texture you’ll find in sourdough. Wild yeast has more flavour than 
+                commercial yeast, and is natural in the sense that it doesn’t contain
+                any additives.Basic sourdough is made from nothing more than flour, water and 
+              salt – here’s what you need to know about each.
+              Flour – sourdough can be made with many types of flour, but for
+              our basic recipe, we’ll be using strong white bread flour. It’s 
+              the most readily available, and means you’ll only need to buy 
+              one flour to start. But, we know ingredients are hard to come 
+              by at the moment, so you can also use strong wholemeal 
+              bread flour.</q>
+        </div>
+        
+            <!-- sliders -->
+           <div class="slider">
+    
+            </div>
+    
+            <div class="article-down">
+                <q> Water – good old tap water is absolutely fine, and it’s the only thing we’ve ever used to make our sourdough. Don’t waste money on minerals. Salt – fine, pure sea salt is the best, but if you don’t have it, you can use any type of cooking
+                    salt you have, except rock salt – that will need to be crushed before using. You can use different flours in our basic recipe without changing the amount of water you use or the method in any way, other than adding less strong white
+                    bread flour. Here are the ideal proportions: For wholemeal, granary, seeded or malted flour use 350g strong white bread flour and 150g of the other flour so you still have 500g total. For rye, spelt, emmer, buckwheat or Khorasan flour
+                    use 400g strong white bread flour and 100g of the other flour so you still have 500g total. You can experiment with different proportions, but bear in mind that other flours will be ‘thirstier’, so you’ll then need to adjust the amount
+                    of water accordingly. But once you know what texture you’re looking for in the dough, this will be easy to do. The basic dough is a blank canvas to which you can add anything you like. Flavouring ingredients should be scattered over
+                    the dough, or worked into it during the final two folds. We’ve added seeds, nuts, grains, olives, herbs, chillies, chunks of cheese and dried fruit to our sourdough.</p>    
+    
+                </div>
+              
+    </div>
+
+
+   <!--<div class="article4-up">
 
 
        <div class="cube-body">
@@ -101,6 +141,6 @@
 
 
    </div>
-</div>
+</div>-->
 
 @endsection
