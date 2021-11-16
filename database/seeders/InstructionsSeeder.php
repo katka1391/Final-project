52d131fd6f7329a7f322c89ae0c9ab146f5c4bef
Katarina Slivova <katka1391@gmail.com>
2021-11-16 21:27:37 +0000

    fixing conflict instruction seeeder

--- database/seeders/InstructionsSeeder.php
+++ database/seeders/InstructionsSeeder.php
@@ -21,14 +21,13 @@ class InstructionsSeeder extends Seeder
  
         $instructions_array = [];
         
-        // looping through the $data array 
+        
         foreach($data as $recipe_data) {
             $step = 1;
-            // looping inside the ingredients key of data array
+            
             foreach($recipe_data["instructions"] as $instructions_data) {
-                // pushing each name of ingredient to the ingredient_array
-                // gives us repeats of names!
-              //  dump($instructions_data["stepNumber"]);
+                
+                //dd($instructions_array);
                 $instructions_array[] = $instructions_data["stepNumber"];
                 $instruction = new Instruction();
                 $instruction->recipe_id = $recipe_data['id'];
@@ -36,9 +35,10 @@ class InstructionsSeeder extends Seeder
                 $instruction->text = $instructions_data["stepNumber"];
                 
                 $instruction->save();
+                // $instructions_array[] = $instructions_data["step"];
+                
             }
         }
-                 //dd($instructions_array);
 
     }
 }
