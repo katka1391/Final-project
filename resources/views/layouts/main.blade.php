039ec32b47bb809fd9012533b304b3292858b423
Katarina Slivova <katka1391@gmail.com>
2021-11-19 16:43:34 +0000

    fixed conflict after merge

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,41 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Martel:wght@300&display=swap" rel="stylesheet">
-    
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    
+
     {{-- <script src="https://kit.fontawesome.com/d59931b590.js" crossorigin="anonymous"></script> --}}
-    
-    <link rel="stylesheet" href=" {{mix ('css/app.css')}}">
-    
-    
-<title>Document</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+
+    <title>Document</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
+
 <body>
     @include('layouts/navBar')
 
 
-    
-    
-    
+
+
+
 
 
     <script src="/script.js"></script>
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    
+
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    
+
     @yield('content')
     @include('layouts/footer')
 </body>
+
 </html>
