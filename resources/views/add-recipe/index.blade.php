e5ba0572dfb6cd9b7e6573fb32a251e4446529d7
Katarina Slivova <katka1391@gmail.com>
2021-11-26 08:37:53 +0000

    styling fomr addrecipe

--- resources/views/add-recipe/index.blade.php
+++ resources/views/add-recipe/index.blade.php
@@ -24,72 +24,72 @@
                     <div class="addRecipe-form1">
                         <label for="name">Name of your recipy:</label>
     
-                        <input type="text" id="name" name="name" required />
+                        <input class="addRecipe-input-nospacearound" type="text" id="name" name="name" required />
     
                         <label for="calories">Calories per portion:</label>
     
-                        <input type="text" id="calories" name="calories" required />
+                        <input class="addRecipe-input-nospacearound" type="text" id="calories" name="calories" required />
     
-                        <label for="cooktime">Cooktime in minutes:</label>
+                        <label  for="cooktime">Cooktime in minutes:</label>
     
-                        <input type="text" id="cooktime" name="cooktime" required />
+                        <input class="addRecipe-input-nospacearound" type="text" id="cooktime" name="cooktime" required />
     
-                        <label for="preptime">Preptime in minutes:</label>
+                        <label  for="preptime">Preptime in minutes:</label>
     
-                        <input type="text" id="preptime" name="preptime" required />
+                        <input class="addRecipe-input-nospacearound" type="text" id="preptime" name="preptime" required />
     
-                        <label for="difficulty">Difficulty, select one from below:</label>
+                        <label  for="difficulty">Difficulty, select one from below:</label>
     
-                        <select id="difficulty" name="difficulty">
+                        <select class="addRecipe-input-nospacearound" id="difficulty" name="difficulty">
                             <option value="Easy">Easy</option>
                             <option value="Medium">Medium</option>
                             <option value="Hard">Hard</option>
                         </select>
     
-                        <label for="servings">For how many people:</label>
+                        <label  for="servings">For how many people:</label>
                        
-                        <input type="text" id="servings" name="servings" required /> <br/>
+                        <input class="addRecipe-input-nospacearound" type="text" id="servings" name="servings" required /> <br/>
     
-                        <label for="fat">Fat:</label>
+                        <label  for="fat">Fat:</label>
                         <br/>
-                        <input type="text" id="fat" name="fat" required />
+                        <input class="addRecipe-input-nospacearound" type="text" id="fat" name="fat" required />
     
     
-                        <label for="carbs">Carbs:</label>
+                        <label  for="carbs">Carbs:</label>
     
-                        <input type="text" id="carbs" name="carbs" required />
+                        <input class="addRecipe-input-nospacearound" type="text" id="carbs" name="carbs" required />
     
-                        <label for="fiber">Fiber:</label>
+                        <label  for="fiber">Fiber:</label>
     
-                        <input type="text" id="fiber" name="fiber" required />
+                        <input class="addRecipe-input-nospacearound" type="text" id="fiber" name="fiber" required />
     
-                        <label for="sugar">Sugar:</label>
+                        <label  for="sugar">Sugar:</label>
     
-                        <input type="text" id="sugar" name="sugar" required />
+                        <input class="addRecipe-input-nospacearound" type="text" id="sugar" name="sugar" required />
     
                         <label for="protein">Protein:</label>
     
-                        <input type="text" id="protein" name="protein" required />
+                        <input class="addRecipe-input-nospacearound" type="text" id="protein" name="protein" required />
     
                         <label for="image">Image url path:</label>
     
-                        <input type="text" id="image" name="image" />
+                        <input class="addRecipe-input-nospacearound" type="text" id="image" name="image" />
     
                     </div>
                     <div class="addRecipe-form2">
                         <label for="ingredients">Ingredients:</label>
     
-                        <input type="text" id="ingredients" name="ingredients[]" required />
-                        <input type="text" id="ingredients" name="ingredients[]" />
-                        <input type="text" id="ingredients" name="ingredients[]" />
+                        <input class="addRecipe-input" type="text" id="ingredients" name="ingredients[]" required />
+                        <input class="addRecipe-input" type="text" id="ingredients" name="ingredients[]" />
+                        <input class="addRecipe-input" type="text" id="ingredients" name="ingredients[]" />
     
-                        <input type="text" id="ingredients" name="ingredients[]" />
-                        <input type="text" id="ingredients" name="ingredients[]" />
-                        <input type="text" id="ingredients" name="ingredients[]" />
+                        <input class="addRecipe-input" type="text" id="ingredients" name="ingredients[]" />
+                        <input class="addRecipe-input" type="text" id="ingredients" name="ingredients[]" />
+                        <input class="addRecipe-input" type="text" id="ingredients" name="ingredients[]" />
     
-                        <input type="text" id="ingredients" name="ingredients[]" />
-                        <input type="text" id="ingredients" name="ingredients[]" />
-                        <input type="text" id="ingredients" name="ingredients[]" />
+                        <input class="addRecipe-input" type="text" id="ingredients" name="ingredients[]" />
+                        <input class="addRecipe-input" type="text" id="ingredients" name="ingredients[]" />
+                        <input class="addRecipe-input" type="text" id="ingredients" name="ingredients[]" />
     
                     </div>
                     <div class="addRecipe-form3">
@@ -97,25 +97,25 @@
                         <label for="instructions">Instructions:</label><br/>
     
                         <label for="instructions">Step 1</label><br/>
-                       <input type="text" id="instructions" name="instructions[]" required />
+                       <input class="addRecipe-input" type="text" id="instructions" name="instructions[]" required />
                        <label for="instructions">Step 2</label><br/>
     
-                       <input type="text" id="instructions" name="instructions[]" />
+                       <input class="addRecipe-input" type="text" id="instructions" name="instructions[]" />
                         <label for="instructions">Step 3</label><br/>
     
-                        <input type="text" id="instructions" name="instructions[]" />
+                        <input class="addRecipe-input" type="text" id="instructions" name="instructions[]" />
                         <label for="instructions">Step 4</label><br/>
     
-                        <input type="text" id="instructions" name="instructions[]" />
+                        <input class="addRecipe-input" type="text" id="instructions" name="instructions[]" />
                         <label for="instructions">Step 5</label><br/>
     
-                         <input type="text" id="instructions" name="instructions[]" />
+                         <input class="addRecipe-input" type="text" id="instructions" name="instructions[]" />
                          <label for="instructions">Step 6</label><br/>
     
-                         <input type="text" id="instructions" name="instructions[]" />
+                         <input class="addRecipe-input" type="text" id="instructions" name="instructions[]" />
                          <label for="instructions">Step 7</label><br/>
     
-                        <input type="text" id="instructions" name="instructions[]" />
+                        <input class="addRecipe-input" type="text" id="instructions" name="instructions[]" />
     
                     </div>
 
